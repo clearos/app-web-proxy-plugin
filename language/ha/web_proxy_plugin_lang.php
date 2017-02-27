18ffb92e40f9ef472a2583663f77881c457ec697
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:34:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy Asiri samar da damar iko ga Web Proxy app.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy Asiri';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy User';
