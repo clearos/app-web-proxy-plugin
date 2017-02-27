0a1cda0873d1153c8e3e7de1a36a433a04d8fe64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:34:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Kebijakan Proxy web menyediakan kontrol akses untuk aplikasi Web Proxy.';
+$lang['web_proxy_plugin_app_name'] = 'Kebijakan Web Proxy';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy Pengguna';
