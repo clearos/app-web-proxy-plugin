4d3cf359700493bd1d54a56794f10ffe7de64c1b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:37:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy Бодлого Web Proxy програм нь хандалтын хяналтыг олгодог.';
+$lang['web_proxy_plugin_app_name'] = 'Вэб Proxy бодлого';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Вэб Proxy хэрэглэгчийн';
