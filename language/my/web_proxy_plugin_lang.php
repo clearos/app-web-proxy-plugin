b546330c4bcd7c1d0049d9ee7aed4edd7db93f37
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:32:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web ကို Proxy မူဝါဒများ Web ကို Proxy app ကိုများအတွက်ဝင်ရောက်ခွင့်ကိုထိန်းချုပ်ပေး။';
+$lang['web_proxy_plugin_app_name'] = 'Web ကို Proxy မူဝါဒများ';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web ကို Proxy အသုံးပြုသူတို့၏';
