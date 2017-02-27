16029ae13b62fcc8d270e17dab4b38f3c07b8df2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:34:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy házirendje eléréskorlátozást a Web Proxy alkalmazást.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy irányelvek';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy felhasználó';
