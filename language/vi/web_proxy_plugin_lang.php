aac32111f1c4d886508fae3953242dedd23c54d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:42:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Proxy Chính sách Web cung cấp kiểm soát truy cập cho các ứng dụng Web Proxy.';
+$lang['web_proxy_plugin_app_name'] = 'Chính sách Web Proxy';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy tài';
