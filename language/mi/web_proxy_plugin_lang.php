1585fd36d7b5eab549292671996983531fa40059
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:37:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'whakarato kaupapa here Takawaenga Tukutuku mana uru mo te taupānga Takawaenga Tukutuku.';
+$lang['web_proxy_plugin_app_name'] = 'Kaupapa here Takawaenga Tukutuku';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Kaiwhakamahi Takawaenga Tukutuku';
