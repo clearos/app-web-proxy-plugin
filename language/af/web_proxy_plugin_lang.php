786341ab0db12922a6327ec7d9a6e58a2b8cd6b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:30:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy Beleid voorsien toegangsbeheer vir die Web Proxy inligting.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy Beleid';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy gebruiker';
