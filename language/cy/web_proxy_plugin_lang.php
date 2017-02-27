72e5eeb204ac822e29210a139160f1aab0875c99
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:42:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Polisïau Proxy gwe yn rhoi rheolaeth mynediad ar gyfer y app Proxy We.';
+$lang['web_proxy_plugin_app_name'] = 'Polisïau Proxy we';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Defnyddiwr trwy Ddirprwy';
