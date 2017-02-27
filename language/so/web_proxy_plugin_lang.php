653ff66b405f937f53b285e6393b400b7bc08c51
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:39:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Siyaasadaha badalka Web bixiyaan helitaanka gacanta for app Web badalka ah.';
+$lang['web_proxy_plugin_app_name'] = 'Siyaasadaha Web badalka';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web badalka User';
