621c04f99b41de5c82310b3ebe721232d49b5145
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:32:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web代理政策为Web代理应用程序访问控制。';
+$lang['web_proxy_plugin_app_name'] = 'Web代理政策';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web代理用户';
