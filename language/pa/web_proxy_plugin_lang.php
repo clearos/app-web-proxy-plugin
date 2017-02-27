c37623dc9e43cda69b685adffae55593f7b1c93e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:38:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'ਵੈੱਬ ਪਰਾਕਸੀ ਡਰਾਇਰ ਵੈੱਬ ਪਰਾਕਸੀ ਐਪ ਲਈ ਪਹੁੰਚ ਕੰਟਰੋਲ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['web_proxy_plugin_app_name'] = 'ਵੈੱਬ ਪਰਾਕਸੀ ਡਰਾਇਰ';
+$lang['web_proxy_plugin_web_proxy_user'] = 'ਵੈੱਬ ਪਰਾਕਸੀ ਉਪਭੋਗੀ';
