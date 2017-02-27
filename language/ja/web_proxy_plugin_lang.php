05246aa0efdf19f8fa7321cd58ff6e42b9ac9723
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:35:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Webプロキシポリシーは、Webプロキシアプリケーションのアクセス制御を提供します。';
+$lang['web_proxy_plugin_app_name'] = 'Webプロキシポリシー';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Webプロキシユーザー';
