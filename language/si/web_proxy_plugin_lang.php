1e6a2eee5ec9ebf0b768f4d58a0fde6020152106
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:39:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'වෙබ් ප්රොක්සි ප්රතිපත්ති වෙබ් ප්රොක්සි යෙදුම සඳහා ප්රවේශ පාලක ලබා දෙයි.';
+$lang['web_proxy_plugin_app_name'] = 'වෙබ් ප්රොක්සි ප්රතිපත්ති';
+$lang['web_proxy_plugin_web_proxy_user'] = 'වෙබ් ප්රොක්සි පරිශීලක';
