0d601271334d166918a490ca8d3a218c196e60d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:39:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy politike zagotavljajo nadzor dostopa za aplikacijo Web Proxy.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy Politike';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy Uporabnik';
