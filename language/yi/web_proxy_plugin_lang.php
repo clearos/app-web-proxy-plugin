87ad602121bb5dcb85d9454264ba8562f9ee41a5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:42:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'וועב פּראָקסי פּאַלאַסיז צושטעלן צוטריט קאָנטראָל פֿאַר די וועב פּראָקסי אַפּ.';
+$lang['web_proxy_plugin_app_name'] = 'וועב פּראָקסי פּאָליסיעס';
+$lang['web_proxy_plugin_web_proxy_user'] = 'וועב פּראָקסי באַניצער';
