786f5cc9ea04be1b1de98d52f265462cb7ceb5fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:40:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'வலை பதிலாள் கொள்கைகள் வலை பதிலாள் பயன்பாட்டை கட்டுப்படுத்த உதவும்.';
+$lang['web_proxy_plugin_app_name'] = 'வலை பதிலாள் கொள்கைகள்';
+$lang['web_proxy_plugin_web_proxy_user'] = 'வலை பதிலாள் பயனர்';
