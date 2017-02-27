60ee78690c86579e8fb296a8724ad488d18baca7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:36:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'បណ្តាញផ្តល់ជូននូវគោលនយោបាយប្រូកស៊ីសម្រាប់ការត្រួតពិនិត្យការចូលដំណើរការបណ្តាញកម្មវិធីប្រូកស៊ី។';
+$lang['web_proxy_plugin_app_name'] = 'គោលនយោបាយប្រូកស៊ីបណ្តាញ';
+$lang['web_proxy_plugin_web_proxy_user'] = 'បណ្តាញរបស់អ្នកប្រើប្រូកស៊ី';
