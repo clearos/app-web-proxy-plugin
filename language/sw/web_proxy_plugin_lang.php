468c5696da79cbb6e9371c8d24db619807ae17ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:40:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Mtandao Sera Wakala kutoa upatikanaji wa kudhibiti kwa programu Mtandao Wakala.';
+$lang['web_proxy_plugin_app_name'] = 'Sera Mtandao Wakala';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Mtandao Wakala mtumiaji';
