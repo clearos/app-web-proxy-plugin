7cfc1801cd5782c9aa76799cd46734fccc161ab4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:42:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Ayelujara aṣoju imulo pese wiwọle iṣakoso fun awọn oju-iwe ayelujara aṣoju app.';
+$lang['web_proxy_plugin_app_name'] = 'Ayelujara aṣoju imulo';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web aṣoju User';
