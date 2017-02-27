aa31da880ac03dc5d5a8d16729ce273780d50ba6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:33:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'TTT Proxy Politikoj havigi aliron kontrolo por la retejo Proxy app.';
+$lang['web_proxy_plugin_app_name'] = 'TTT Proxy Politikoj';
+$lang['web_proxy_plugin_web_proxy_user'] = 'TTT Proxy Uzanto';
