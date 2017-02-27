5441b38137cce47a9d6fee854b34cd95994666bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:37:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'वेब प्रॉक्सी धोरणे वेब प्रॉक्सी अॅप साठी प्रवेश नियंत्रण प्रदान.';
+$lang['web_proxy_plugin_app_name'] = 'वेब प्रॉक्सी धोरणे';
+$lang['web_proxy_plugin_web_proxy_user'] = 'वेब प्रॉक्सी वापरकर्ता';
