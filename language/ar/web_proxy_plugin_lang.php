82a8bfa1105239de2643c517d48a602c709079f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:31:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'وتوفر سياسات وكيل ويب التحكم في الوصول لهذا التطبيق وكيل ويب.';
+$lang['web_proxy_plugin_app_name'] = 'سياسات وكيل ويب';
+$lang['web_proxy_plugin_web_proxy_user'] = 'وكيل العضو الويب';
