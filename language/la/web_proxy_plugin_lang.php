c9646fa8d7f11e93111df7295eea93c945fb3d48
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:36:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Imperium providere accessum ad Web Proxy proxy Policies App.';
+$lang['web_proxy_plugin_app_name'] = 'Ergo Web Politiae';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Ergo User Web';
