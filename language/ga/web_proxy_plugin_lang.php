a5fdcf821ee0cfda015e3162f098019f45525044
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:35:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'a chur ar fáil Polasaithe Proxy Gréasáin rialaithe rochtana don app Proxy Gréasáin.';
+$lang['web_proxy_plugin_app_name'] = 'Polasaithe Proxy Gréasáin';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy Úsáideoir';
