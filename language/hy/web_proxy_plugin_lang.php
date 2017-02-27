11fe03bf9a22aa73b84e8c9ffc5cea0792775c2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:31:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy քաղաքականությունը տրամադրել մուտքի վերահսկման համար էկրանային Proxy հավելվածի:';
+$lang['web_proxy_plugin_app_name'] = 'Վեբ Proxy քաղաքականություն';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Վեբ Proxy Օգտվող';
