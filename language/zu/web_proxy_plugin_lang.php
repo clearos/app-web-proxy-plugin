6a61d4c9ee9dae9ccff8030ebcfe97c932cf314d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:42:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy Izinqubomgomo ukuhlinzekela nokulawula ukufinyelela lokusebenza Web Proxy.';
+$lang['web_proxy_plugin_app_name'] = 'Izinqubomgomo Web Proxy';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy Umsebenzisi';
