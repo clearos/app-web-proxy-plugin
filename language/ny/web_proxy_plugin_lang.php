1486116bd03c820b5f08b09291388ab11003cf52
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:32:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Ndondomeko tidzakulowereni imapatsa kulamulira kwa app Web tidzakulowereni.';
+$lang['web_proxy_plugin_app_name'] = 'Ndondomeko Web tidzakulowereni';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web tidzakulowereni Wosuta';
