04b2478d3fbdf3e4a656b5e941a6abfbf75b389d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:37:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy Policies dia manome fahafahana ho an&#39;ny fanaraha-maso ny Web Proxy App.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy Policies';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy User';
