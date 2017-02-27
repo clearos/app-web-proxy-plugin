5e87b789b1cac55d5a5a988ebe7b25d2a2ef95c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:37:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Polisi Proksi Web menyediakan kawalan akses untuk aplikasi Web Proksi.';
+$lang['web_proxy_plugin_app_name'] = 'Polisi Web Proxy';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy Pengguna';
