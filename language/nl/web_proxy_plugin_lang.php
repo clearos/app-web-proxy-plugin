cabac592d486d94743c5670386f543cf7c0fafaf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:38:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy Beleid bieden toegangscontrole voor de Web Proxy app.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy Beleid';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy Gebruiker';
