dd0787fc9831663ef8258fa1fb51b0b3d9b18657
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:36:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = '웹 프록시 정책은 웹 프록시 응용 프로그램에 대한 액세스 제어를 제공합니다.';
+$lang['web_proxy_plugin_app_name'] = '웹 프록시 정책';
+$lang['web_proxy_plugin_web_proxy_user'] = '웹 프록시 사용';
