88c561eafdf26fb0191b64c3c643d69dc45ceb88
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:34:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Πολιτικές μεσολάβησης παρέχει έλεγχο πρόσβασης για την εφαρμογή Web μεσολάβησης.';
+$lang['web_proxy_plugin_app_name'] = 'Πολιτικές μεσολάβησης Web';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Μεσολάβησης Web User';
