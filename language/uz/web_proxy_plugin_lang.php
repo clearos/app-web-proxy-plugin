471fa0a1bdd8b6a3147abb06415e6fd9a163a52c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:42:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Veb Proksi siyosat Veb Proksi ilova uchun erkin foydalanish nazorat beradi.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy siyosat';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy Foydalanuvchi';
