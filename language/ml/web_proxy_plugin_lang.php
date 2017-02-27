a8205e66380afe56b475a5435073c7f824abd72c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:37:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'വെബ് പ്രോക്സി നയങ്ങള് വെബ് പ്രോക്സി അപ്ലിക്കേഷൻ ആക്സസ്സ് നിയന്ത്രണം നൽകുന്നു.';
+$lang['web_proxy_plugin_app_name'] = 'വെബ് പ്രോക്സി നയങ്ങള്';
+$lang['web_proxy_plugin_web_proxy_user'] = 'വെബ് പ്രോക്സി ഉപയോക്താവ്';
