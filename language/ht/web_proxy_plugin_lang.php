ccd7cfdebd51e346c169eacf087913bfdb53869b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:34:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Règleman Proxy bay kontwòl aksè pou app a entènèt Proxy.';
+$lang['web_proxy_plugin_app_name'] = 'Règleman Web Proxy';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy Itilizatè';
