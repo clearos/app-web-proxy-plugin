1df3ea139e858909313850fcfacd8595a8a23624
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:41:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'นโยบาย Web Proxy ให้การควบคุมการเข้าถึงสำหรับ Web Proxy App';
+$lang['web_proxy_plugin_app_name'] = 'นโยบาย Web Proxy';
+$lang['web_proxy_plugin_web_proxy_user'] = 'ผู้ใช้เว็บพร็อกซี่';
