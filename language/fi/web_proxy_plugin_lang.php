07696ef1c96e25965bc2e689885f2d53a3b45694
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:33:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy käytännöt tarjoavat kulunvalvonta Web Proxy sovellus.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy käytännöt';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy User';
