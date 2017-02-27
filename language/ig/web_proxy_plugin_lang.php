f04fb8d27d56927a19ac74c70d3a5ae491a4f12b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:35:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy Atumatu-enye ohere akara maka Web Proxy ngwa.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy Atumatu';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy User';
