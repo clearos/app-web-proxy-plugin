6decf79c70ecb5ca55c82abb1a51061fbbf53c2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:41:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'వెబ్ ప్రాక్సీ విధానాలు వెబ్ ప్రాక్సీ అనువర్తనం ప్రాప్యత నియంత్రణను అందిస్తాయి.';
+$lang['web_proxy_plugin_app_name'] = 'వెబ్ ప్రాక్సీ విధానాలు';
+$lang['web_proxy_plugin_web_proxy_user'] = 'వెబ్ ప్రాక్సీ వాడుకరి';
