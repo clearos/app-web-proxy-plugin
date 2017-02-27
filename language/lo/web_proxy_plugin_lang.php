9e13adef028eb46f72f9a21cc91503e7f5573d2a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:36:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'ອັນນະໂຍບາຍຕົວແທນໃຫ້ການຄວບຄຸມການເຂົ້າເຖິງສໍາລັບ app Web Proxy.';
+$lang['web_proxy_plugin_app_name'] = 'ນະໂຍບາຍອັນ Proxy';
+$lang['web_proxy_plugin_web_proxy_user'] = 'ຜູ້ໃຊ້ເວັບ Proxy';
