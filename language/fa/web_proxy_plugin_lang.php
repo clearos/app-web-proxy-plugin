e2705c1475bbf1890ded0e676bacee5a84776ee6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:38:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'قوانین پروکسی وب کنترل دسترسی برای برنامه پروکسی وب.';
+$lang['web_proxy_plugin_app_name'] = 'قوانین پروکسی وب';
+$lang['web_proxy_plugin_web_proxy_user'] = 'وب پروکسی کاربر';
