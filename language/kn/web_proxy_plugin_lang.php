6e96cee171d36d20e6beff5818510de567464227
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:36:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'ವೆಬ್ ಪ್ರಾಕ್ಸಿ ನೀತಿಗಳು ವೆಬ್ ಪ್ರಾಕ್ಸಿ ಅಪ್ಲಿಕೇಶನ್ ಆಕ್ಸೆಸ್ ಕಂಟ್ರೋಲ್ ನೀಡಲಾಗುತ್ತದೆ.';
+$lang['web_proxy_plugin_app_name'] = 'ವೆಬ್ ಪ್ರಾಕ್ಸಿ ನೀತಿಗಳು';
+$lang['web_proxy_plugin_web_proxy_user'] = 'ವೆಬ್ ಪ್ರಾಕ್ಸಿ ಬಳಕೆದಾರ';
