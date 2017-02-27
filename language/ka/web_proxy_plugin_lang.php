d24f416028d17b66ddfdffaa4c7ffd176dfab613
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:35:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Proxy პოლიტიკის უზრუნველყოს კონტროლის Web Proxy app.';
+$lang['web_proxy_plugin_app_name'] = 'Web Proxy პოლიტიკა';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Proxy მომხმარებელი';
