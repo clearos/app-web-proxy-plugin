ffcf7e2602da8fec35fc87f3b8ee3e4f76642228
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:40:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Web Maano proksi fana phihlello ya taolo ya bakeng sa app e Web proksi.';
+$lang['web_proxy_plugin_app_name'] = 'Maano Web proksi';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web proksi User';
