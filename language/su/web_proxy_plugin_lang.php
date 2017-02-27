b003ac079f42368df1bdd83eabe1f9be035f9428
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:40:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Kawijakan proxy Web nyadiakeun kadali aksés pikeun aplikasi Web proxy.';
+$lang['web_proxy_plugin_app_name'] = 'Kawijakan Web proxy';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web proxy Pamaké';
