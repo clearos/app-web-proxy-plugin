8731db215bd19ee74b26fa8337dddbb4ec694ef4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:41:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'Сиёсати мустаќим Web таъмини идоракунии дастрасӣ, барои барнома Ваколатномаи Web мебошад.';
+$lang['web_proxy_plugin_app_name'] = 'Сиёсати мустаќим Web';
+$lang['web_proxy_plugin_web_proxy_user'] = 'Web Ваколатномаи Корбар';
