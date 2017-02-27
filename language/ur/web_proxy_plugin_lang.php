9fa3cafa2c13bdbd13be8d42efbbb43aabbd3526
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:41:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'ویب پراکسی پالیسیاں ویب پراکسی اپلی کیشن کے لئے رسائی کے کنٹرول فراہم.';
+$lang['web_proxy_plugin_app_name'] = 'ویب پراکسی پالیسیاں';
+$lang['web_proxy_plugin_web_proxy_user'] = 'ویب پراکسی صارف';
