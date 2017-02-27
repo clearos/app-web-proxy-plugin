43d2f943cb2f492a2aca3dac1a5c76607056286e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:34:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/web_proxy_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['web_proxy_plugin_app_description'] = 'વેબ પ્રોક્સી નીતિ વેબ પ્રોક્સી એપ્લિકેશન માટે ઍક્સેસ નિયંત્રણ પૂરું પાડે છે.';
+$lang['web_proxy_plugin_app_name'] = 'વેબ પ્રોક્સી નીતિ';
+$lang['web_proxy_plugin_web_proxy_user'] = 'વેબ પ્રોક્સી વપરાશકર્તા';
